10691617ddcc2ee18360f020358b381139d6f16b
Sergei <garibzyanov@gmail.com>
2020-10-14 21:13:54 +0500

    add solution for 1+.js

--- 1+.js
+++ 1+.js
@@ -7,25 +7,24 @@ http.send();
 
 http.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        console.log(http.responseText);
+        let data = JSON.parse(http.responseText);
+        let unixtime = data['unixtime'];
+
+        let date = new Date(unixtime * 1000),
+            hour = date.getHours(),
+            username = 'Серёжа';
+    
+        if (hour >= 0 && hour <= 4) {
+            console.log(`Доброй ночи, ${username}`);
+        }
+        if (hour >= 5 && hour <= 9) {
+            console.log(`Доброе утро, ${username}`);
+        }
+        if (hour >= 10 && hour <= 16) {
+            console.log(`Добрый день, ${username}`);
+        }
+        else {
+            console.log(`Добрый вечер, ${username}`);
+        }
     }
 };
-
-
-
-let date = new Date(),
-    hour = date.getHours(),
-    username = 'Серёжа';
-
-if (hour >= 0 && hour <= 4) {
-    console.log(`Доброй ночи, ${username}`);
-}
-if (hour >= 5 && hour <= 9) {
-    console.log(`Доброе утро, ${username}`);
-}
-if (hour >= 10 && hour <= 16) {
-    console.log(`Добрый день, ${username}`);
-}
-else {
-    console.log(`Добрый вечер, ${username}`);
-}
